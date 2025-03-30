14686b9b6bf9e326bde4bb6357c72daf55d206ef
Andrew D Smith <andrewds@usc.edu>
2025-03-30 16:57:21 -0700

    mylib.cpp mylib.hpp : adding lib sources

---
+++ mylib.cpp
@@ -0,0 +1,7 @@
+#include "mylib.hpp"
+#include <print>
+
+void
+say_hi() {
+  std::print("HI!\n");
+}
---
+++ mylib.hpp
@@ -0,0 +1,7 @@
+#ifndef MYLIB_HPP_
+#define MYLIB_HPP_
+
+void
+say_hi();
+
+#endif  // MYLIB_HPP_
