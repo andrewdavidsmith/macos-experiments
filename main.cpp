93672f2242026ff19d39a812055f53a28e4970d3
Andrew D Smith <andrewds@usc.edu>
2026-08-15 18:24:41 -0700

    Add range-based enumeration and printing

--- main.cpp
+++ main.cpp
@@ -1,6 +1,12 @@
 #include "mylib.hpp"
+#include <print>
+#include <ranges>
+
 int
 main() {
   say_hi();
+  for (const auto a : std::views::enumerate(10)) {
+    std::println("{}", a);
+  }
   return 0;
 }
